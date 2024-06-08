3b00e03b21d5d13da2cccc2b1f3862f1851f0b05
marina6coneto <santoscmarina@hotmail.com>
2024-06-08 11:46:28 -0300

    funções

---
+++ Funcoes.java
@@ -0,0 +1,11 @@
+public class Funcoes {
+    public static void main(String[] args) {
+        String nomeOriginal = "Let's code";
+        saudacao(nomeOriginal);
+    }
+
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
+    }
+
+}
