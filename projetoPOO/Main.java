93cf797f8df4df5fa321827ea7757d578cdf225c
marina6coneto <santoscmarina@hotmail.com>
2024-06-10 13:26:23 -0300

    POO em java

---
+++ projetoPOO/Main.java
@@ -0,0 +1,16 @@
+package projetoPOO;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        byte variavelByte = 127;
+        System.out.println(variavelByte);
+    }
+}
+
+
+// byte -127 até 127
+// short -32768 até 32767
+// char 0 até 65535 
+// int -2b até 2b
+// long -9t até 9t
